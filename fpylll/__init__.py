8f910dc8dd280619a5457e1ce4fc8e81e70ee666
Martin R. Albrecht <martinralbrecht@googlemail.com>
2015-11-03 21:30:20 +0000

    fix includes

--- fpylll/__init__.py
+++ fpylll/__init__.py
@@ -1,5 +1,5 @@
 from integer_matrix import IntegerMatrix
-from gso import MatGSO, GSO_DEFAULT, GSO_INT_GRAM, GSO_ROW_EXPO, GSO_OP_FORCE_LONG
+from gso import MatGSO
 from lll import lll_reduction, LLLReduction, LLL_VERBOSE, LLL_EARLY_RED, LLL_SIEGEL
 from wrapper import Wrapper
 from enumerate import Enumeration
