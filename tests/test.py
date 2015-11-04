a3064c02f24ce0ceae165be593f87ea0af855e41
Martin R. Albrecht <martinralbrecht@googlemail.com>
2015-11-04 00:14:56 +0000

    nicer example

--- tests/test.py
+++ tests/test.py
@@ -2,8 +2,8 @@ from fpylll import *
 
 from copy import copy
 
-A = IntegerMatrix(50, 50)
-A.randomize("ntrulike", bits=50, q=127)
+A = IntegerMatrix(100, 100)
+A.randomize("ntrulike", bits=200, q=127)
 
 B = copy(A)
-bkz_reduction(B, 20)
+bkz_reduction(B, 20, verbose=True)
