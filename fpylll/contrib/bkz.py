26d2f567d460f79c808ba629203b295c612f042d
Martin R. Albrecht <martinralbrecht@googlemail.com>
2015-11-08 12:13:36 +0000

    extended contrib.bkz

--- fpylll/contrib/bkz.py
+++ fpylll/contrib/bkz.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-
 import time
 
 from fpylll import IntegerMatrix, GSO, LLL
 from fpylll import BKZ
 from fpylll import Enumeration as Enum
-from fpylll import gso
+from fpylll import EnumerationError
+from bkz_stats import BKZStats
+
 
 class BKZReduction:
     def __init__(self, A):
@@ -35,6 +36,8 @@ class BKZReduction:
         :rtype:
 
         """
+        stats = BKZStats(self, verbose=param.flags & BKZ.VERBOSE)
+
         if param.flags & BKZ.AUTO_ABORT:
             auto_abort = BKZ.AutoAbort(self.m, self.A.nrows)
 
@@ -44,7 +47,8 @@ class BKZReduction:
 
         i = 0
         while True:
-            clean = self.bkz_loop(param, 0, self.A.nrows)
+            with stats.context("tour"):
+                clean = self.tour(param, 0, self.A.nrows, stats)
             if clean or param.block_size >= self.A.nrows:
                 break
             if (param.flags & BKZ.AUTO_ABORT) and auto_abort.test_abort():
@@ -55,7 +59,7 @@ class BKZReduction:
                 break
             i += 1
 
-    def bkz_loop(self, param, min_row, max_row):
+    def tour(self, param, min_row, max_row, stats=None):
         """FIXME! briefly describe function
 
         :param block_size:
@@ -68,10 +72,12 @@ class BKZReduction:
         clean = True
         for kappa in range(min_row, max_row-1):
             block_size = min(param.block_size, max_row - kappa)
-            clean &= self.svp_reduction(kappa, param, block_size)
+            clean &= self.svp_reduction(kappa, param, block_size, stats)
+            if stats:
+                stats.log_clean_kappa(kappa, clean)
         return clean
 
-    def svp_reduction(self, kappa, param, block_size):
+    def svp_reduction(self, kappa, param, block_size, stats=None):
         """FIXME! briefly describe function
 
         :param kappa:
@@ -82,67 +88,78 @@ class BKZReduction:
         """
         clean = True
 
-        self.lll_obj(0, kappa, kappa + block_size)
-        if self.lll_obj.nswaps > 0:
-            clean = False
+        if stats is None:
+            stats = BKZStats(self)
 
-        #  preprocessing
-        if param.preprocessing:
-            preproc = param.preprocessing
-            auto_abort = BKZ.AutoAbort(self.m, kappa + block_size, kappa)
-            cputime_start = time.clock()
+        with stats.context("preproc"):
+            self.lll_obj(0, kappa, kappa + block_size)
+            if self.lll_obj.nswaps > 0:
+                clean = False
 
-            i = 0
-            while True:
-                clean_inner = self.bkz_loop(preproc, kappa, kappa + block_size)
-                if clean_inner:
-                    break
-                else:
-                    clean = clean_inner
-                if auto_abort.test_abort():
-                    break
-                if (preproc.flags & BKZ.MAX_LOOPS) and i >= preproc.max_loops:
-                    break
-                if (preproc.flags & BKZ.MAX_TIME) and time.clock() - cputime_start >= preproc.max_time:
-                    break
-                i += 1
+            #  preprocessing
+            if param.preprocessing:
+                preproc = param.preprocessing
+                auto_abort = BKZ.AutoAbort(self.m, kappa + block_size, kappa)
+                cputime_start = time.clock()
+
+                i = 0
+                while True:
+                    clean_inner = self.tour(preproc, kappa, kappa + block_size)
+                    if clean_inner:
+                        break
+                    else:
+                        clean = clean_inner
+                    if auto_abort.test_abort():
+                        break
+                    if (preproc.flags & BKZ.MAX_LOOPS) and i >= preproc.max_loops:
+                        break
+                    if (preproc.flags & BKZ.MAX_TIME) and time.clock() - cputime_start >= preproc.max_time:
+                        break
+                    i += 1
 
         max_dist, expo = self.m.get_r_exp(kappa, kappa)
         delta_max_dist = self.lll_obj.delta * max_dist
 
         # TODO: Gaussian heuristic
-
-        solution, max_dist = Enum.enumerate(self.m, max_dist, expo,
-                                            kappa, kappa + block_size,
-                                            param.pruning)
+        with stats.context("svp"):
+            try:
+                solution, max_dist = Enum.enumerate(self.m, max_dist, expo,
+                                                    kappa, kappa + block_size,
+                                                    param.pruning)
+            except EnumerationError, msg:
+                if param.flags & BKZ.GH_BND:
+                    return clean
+                else:
+                    raise EnumerationError(msg)
 
         if max_dist >= delta_max_dist:
             return clean
 
         nonzero_vectors = len([x for x in solution if x])
 
-        if nonzero_vectors == 1:
-            first_nonzero_vector = None
-            for i in range(block_size):
-                if abs(solution[i]) == 1:
-                    first_nonzero_vector = i
-                    break
-
-            self.m.move_row(kappa + first_nonzero_vector, kappa)
-            self.lll_obj.size_reduction(kappa, kappa + 1)
-
-        else:
-            d = self.m.d
-            self.m.create_row()
-
-            with self.m.row_ops(d, d+1):
+        with stats.context("postproc"):
+            if nonzero_vectors == 1:
+                first_nonzero_vector = None
                 for i in range(block_size):
-                    self.m.row_addmul(d, kappa + i, solution[i])
+                    if abs(solution[i]) == 1:
+                        first_nonzero_vector = i
+                        break
 
-            self.m.move_row(d, kappa)
-            self.lll_obj(kappa, kappa, kappa + block_size + 1)
-            self.m.move_row(kappa + block_size, d)
+                self.m.move_row(kappa + first_nonzero_vector, kappa)
+                self.lll_obj.size_reduction(kappa, kappa + 1)
 
-            self.m.remove_last_row()
+            else:
+                d = self.m.d
+                self.m.create_row()
+
+                with self.m.row_ops(d, d+1):
+                    for i in range(block_size):
+                        self.m.row_addmul(d, kappa + i, solution[i])
+
+                self.m.move_row(d, kappa)
+                self.lll_obj(kappa, kappa, kappa + block_size + 1)
+                self.m.move_row(kappa + block_size, d)
+
+                self.m.remove_last_row()
 
         return False
---
+++ fpylll/contrib/bkz_stats.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+import time
+from collections import OrderedDict
+
+
+class BKZStatsContext:
+    def __init__(self, stats, what):
+        self.stats = stats
+        self.what = what
+
+    def __enter__(self):
+        self.timestamp = time.clock()
+
+        if self.what == "tour":
+            self.stats.tour_begin()
+        elif self.what == "preproc":
+            self.stats.preproc_begin()
+        elif self.what == "svp":
+            self.stats.svp_begin()
+        elif self.what == "postproc":
+            self.stats.postproc_begin()
+        else:
+            raise NotImplementedError
+
+    def __exit__(self, exception_type, exception_value, exception_traceback):
+        time_spent = time.clock() - self.timestamp
+        if self.what == "tour":
+            self.stats.tour_end(time_spent)
+        elif self.what == "preproc":
+            self.stats.preproc_end(time_spent)
+        elif self.what == "svp":
+            self.stats.svp_end(time_spent)
+        elif self.what == "postproc":
+            self.stats.postproc_end(time_spent)
+        else:
+            raise NotImplementedError
+
+
+class BKZStats:
+    def __init__(self, bkz, verbose=False):
+        self.bkz = bkz
+        self.verbose = verbose
+        self.i = 0
+        self.tours = {}
+
+    def context(self, what):
+        return BKZStatsContext(self, what)
+
+    def tour_begin(self):
+        i = self.i
+        self.tours[i] = OrderedDict([("i", i),
+                                     ("total time", 0.0),
+                                     ("time", 0.0),
+                                     ("preproc time", 0.0),
+                                     ("svp time", 0.0),
+                                     ("postproc time", 0.0),
+                                     ("r_0", 0.0),
+                                     ("slope", 0.0),
+                                     ("max(kappa)", 0)])
+
+        if i > 0:
+            self.tours[i]["total time"] = self.tours[i-1]["total time"]
+            self.tours[i]["max(kappa)"] = self.tours[i-1]["max(kappa)"]
+
+    def tour_end(self, time):
+        i = self.i
+        self.tours[i]["time"] = time
+        r, exp = self.bkz.m.get_r_exp(0, 0)
+        self.tours[i]["r_0"] = r * 2 **exp
+        self.tours[i]["slope"] = self.bkz.m.get_current_slope(0, self.bkz.A.nrows)
+        self.tours[i]["total time"] += self.tours[i]["time"]
+
+        if self.verbose:
+            print unicode(self)
+        self.i += 1
+
+    def preproc_begin(self):
+        if self.i not in self.tours:
+            self.tour_begin()
+
+    def preproc_end(self, time):
+        self.tours[self.i]["preproc time"] += time
+
+    def svp_begin(self):
+        if self.i not in self.tours:
+            self.tour_begin()
+
+    def svp_end(self, time):
+        self.tours[self.i]["svp time"] += time
+
+    def postproc_begin(self):
+        if self.i not in self.tours:
+            self.tour_begin()
+
+    def postproc_end(self, time):
+        i = self.i
+        self.tours[i]["postproc time"] += time
+
+    def __unicode__(self):
+        s = []
+        tour = self.tours[self.i]
+        s.append(u"\"i\": %3d"%self.i)
+        s.append(u"\"total\": %9.2f"%(tour["total time"]))
+        s.append(u"\"time\": %8.2f"%(tour["time"]))
+        s.append(u"\"preproc\": %8.2f"%(tour["preproc time"]))
+        s.append(u"\"svp\": %8.2f"%(tour["svp time"]))
+        s.append(u"\"r_0\": %.4e"%(tour["r_0"]))
+        s.append(u"\"slope\": %7.4f"%(tour["slope"]))
+        s.append(u"\"max(κ)\": %3d"%(tour["max(kappa)"]))
+        return u"{" + u",  ".join(s) + u"}"
+
+    def __str__(self):
+        u = unicode(self)
+        return u.replace(u"κ", "kappa")
+
+    def log_clean_kappa(self, kappa, clean):
+        if clean and self.tours[self.i]["max(kappa)"] < kappa:
+            self.tours[self.i]["max(kappa)"] = kappa
