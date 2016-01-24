36763ac707d66d7a1daf75032bf51ed2a10022b0
Michael Walter <michael.walter@swel.com>
2016-01-23 18:13:45 -0800

    dbkz loop done. only missing dsvp reduction

--- src/fpylll/contrib/simple_bkz.py
+++ src/fpylll/contrib/simple_bkz.py
@@ -110,3 +110,63 @@ class BKZReduction:
             self.m.remove_last_row()
 
         return False
+
+class DBKZReduction(BKZReduction):
+    
+    def bkz_loop(self, block_size, min_row, max_row):
+        clean = True
+        for kappa in range(max_row - block_size, min_row - 1, -1):
+            clean &= self.svp_reduction(kappa, block_size)
+        clean &= BKZReduction.bkz_loop(self, block_size, min_row, max_row)
+        return clean
+
+    def dsvp_reduction(self, kappa, block_size):
+        """FIXME! briefly describe function
+
+        :param kappa:
+        :param block_size:
+        :returns:
+        :rtype:
+
+        """
+        clean = True
+
+        self.lll_obj(0, kappa, kappa + block_size)
+        if self.lll_obj.nswaps > 0:
+            clean = False
+
+        max_dist, expo = self.m.get_r_exp(kappa, kappa)
+        delta_max_dist = self.lll_obj.delta * max_dist
+
+        solution, max_dist = Enum.enumerate(self.m, max_dist, expo, kappa, kappa + block_size, None)
+
+        if max_dist >= delta_max_dist:
+            return clean
+
+        nonzero_vectors = len([x for x in solution if x])
+
+        if nonzero_vectors == 1:
+            first_nonzero_vector = None
+            for i in range(block_size):
+                if abs(solution[i]) == 1:
+                    first_nonzero_vector = i
+                    break
+
+            self.m.move_row(kappa + first_nonzero_vector, kappa)
+            self.lll_obj.size_reduction(kappa, kappa + 1)
+
+        else:
+            d = self.m.d
+            self.m.create_row()
+
+            with self.m.row_ops(d, d+1):
+                for i in range(block_size):
+                    self.m.row_addmul(d, kappa + i, solution[i])
+
+            self.m.move_row(d, kappa)
+            self.lll_obj(kappa, kappa, kappa + block_size + 1)
+            self.m.move_row(kappa + block_size, d)
+
+            self.m.remove_last_row()
+
+        return False
