ca1a5f803f4d3d97e3922ef20d71f667871ed623
Guillaume Bonnoron <guillaume.bonnoron@gmail.com>
2016-07-22 13:41:24 +0200

    Adding CVP to fpylll API

--- src/fpylll/__init__.py
+++ src/fpylll/__init__.py
@@ -7,6 +7,7 @@ from .fplll.enumeration import Enumeration, EnumerationError
 from .fplll.bkz import BKZ
 from .fplll.bkz_param import load_strategies_json
 from .fplll.svpcvp import SVP
+from .fplll.svpcvp import CVP
 from .util import ReductionError
 from .util import set_random_seed, set_precision, get_precision
 from .config import default_strategy
