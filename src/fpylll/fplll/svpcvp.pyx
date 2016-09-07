5c73a21f05ec30008950a86a3c882562a1dc563e
Guillaume Bonnoron <guillaume.bonnoron@gmail.com>
2016-09-07 15:56:04 +0200

    Fix doctest for python 3

--- src/fpylll/fplll/svpcvp.pyx
+++ src/fpylll/fplll/svpcvp.pyx
@@ -104,7 +104,7 @@ def closest_vector(IntegerMatrix B, target, int flags=CVP_DEFAULT):
     >>> A = IntegerMatrix.random(5, 'uniform', bits=10)
     >>> lll = LLL.reduction(A)
     >>> t = (94, -42, 123, 512, -1337)
-    >>> print CVP.closest_vector(A, t)
+    >>> print (CVP.closest_vector(A, t))
     (-34, 109, 204, 360, -1548)
 
     """
