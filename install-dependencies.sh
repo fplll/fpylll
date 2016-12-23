3cb15ac288da09d7f91702bffa4841edaa8db6d7
Martin R. Albrecht <martinralbrecht@googlemail.com>
2016-12-23 11:01:46 +0000

    clone matching fplll branch if it exists in install-dependencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-git clone https://github.com/dstehle/fplll
 
-cd fplll
+if [ "$TRAVIS_BRANCH" != "" ]; then
+    FPLLL_BRANCH=$TRAVIS_BRANCH;
+fi
+
+if [ "$FPLLL_BRANCH" = "" ]; then
+    FPLLL_BRANCH=master
+fi;
+
+cloned=$(git clone https://github.com/fplll/fplll -b "$FPLLL_BRANCH")
+
+if [ "$cloned" -ne 0 ]; then
+    git clone https://github.com/fplll/fplll
+fi
+
+cd fplll || exit
 ./autogen.sh
-if test "$1" != ""; then
+
+if [ "$1" != "" ]; then
     ./configure --prefix="$1"
 else
     ./configure
