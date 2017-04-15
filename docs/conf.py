9907962bb3a65f182acf1b680aa2f64e35133d4c
Martin R. Albrecht <martinralbrecht@googlemail.com>
2017-04-15 09:59:13 +0100

    copyright

--- docs/conf.py
+++ docs/conf.py
@@ -54,8 +54,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'fpylll'
-copyright = u'2015, Martin R. Albrecht'
-author = u'Martin R. Albrecht'
+copyright = u'2015-2017, fpylll team'
+author = u'fpylll team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
