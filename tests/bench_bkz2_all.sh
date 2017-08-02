8af3734f35dea3d1a4cebc94c18c48ac47aded3f
Leo Ducas <ducas@cwi.nl>
2017-08-02 09:57:36 +0200

    rm sh script that change at each experiment

--- tests/bench_bkz2_all.sh
+++
@@ -1,8 +0,0 @@
-python -u bench_bkz2_otf.py .70 > bkz2_otf_p70_noGHbAtAll.txt &
-python -u bench_bkz2_otf.py .30 > bkz2_otf_p30_noGHbAtAll.txt &
-python -u bench_bkz2_otf.py .15 > bkz2_otf_p15_noGHbAtAll.txt &
-python -u bench_bkz2_otf.py .07 > bkz2_otf_p07_noGHbAtAll.txt &
-
-sleep 2
-
-tail -f bkz2_otf_p*.txt
