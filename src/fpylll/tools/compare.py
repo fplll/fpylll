f3d2a44328d30ec075ff6278e3c2e3a9b56a9f71
Martin R. Albrecht <martinralbrecht@googlemail.com>
2017-08-08 19:17:47 +0100

    fix BKZ2_otf not working bug

--- src/fpylll/tools/compare.py
+++ src/fpylll/tools/compare.py
@@ -24,6 +24,7 @@ import logging
 import copy
 import fpylll.algorithms.bkz
 import fpylll.algorithms.bkz2
+import fpylll.algorithms.bkz2_otf
 
 
 # Utility Functions
@@ -168,7 +169,8 @@ class BKZ2(fpylll.algorithms.bkz2.BKZReduction):
                                strategies=BKZ.DEFAULT_STRATEGY)
         return fpylll.algorithms.bkz2.BKZReduction.tour(self, params, tracer=dummy_tracer)
 
-class BKZ2_otf(fpylll.algorithms.bkz2.BKZReduction):
+
+class BKZ2_otf(fpylll.algorithms.bkz2_otf.BKZReduction):
     def tour(self, params, min_row=0, max_row=-1, tracer=dummy_tracer):
         if isinstance(params, int):
             params = BKZ.Param(block_size=params,
